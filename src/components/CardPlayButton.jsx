94dcc4ae0dc705b4114ba7ca65ae30c84558e028
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2025-08-05 00:11:09 -0500

    feat: add CardPlayButton component and integrate it into PlayListItemCard

---
+++ src/components/CardPlayButton.jsx
@@ -0,0 +1,9 @@
+import { Play, Pause } from "./Player";
+
+export default function CardPlayButton({ id }) {
+  return (
+    <div className="card-play-button rounded-full bg-green-500 p-4">
+      <Play />
+    </div>
+  );
+}
