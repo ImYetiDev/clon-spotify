2f3ce24ec7f31667b673c583e9eb970459148505
ImYetiDev <152050162+ImYetiDev@users.noreply.github.com>
2025-08-06 22:55:15 -0500

    feat: implement player state management with zustand and update CardPlayButton integration

--- src/components/CardPlayButton.jsx
+++ src/components/CardPlayButton.jsx
@@ -1,9 +1,16 @@
 import { Play, Pause } from "./Player";
+import { usePlayerStore } from "../store/playerStore.js";
 
 export default function CardPlayButton({ id }) {
+  const { isPlaying, setIsPlaying , currentSong, setCurrentSong } = usePlayerStore(state => state);
+
+  const handlePlayPause = () => {
+    setIsPlaying(!isPlaying);
+  };
+
   return (
-    <div className="card-play-button rounded-full bg-green-500 p-4">
-      <Play />
-    </div>
+    <button className="card-play-button rounded-full bg-green-500 p-4" onClick={handlePlayPause}>
+      {isPlaying ? <Pause /> : <Play />}
+    </button>
   );
 }
---
+++ src/store/playerStore.js
@@ -0,0 +1,9 @@
+import { create } from "zustand";
+
+export const usePlayerStore = create((set) => ({
+  isPlaying: false,
+  currentSong: { playlist: null, song: null , songs: [] },
+  setIsPlaying: (isPlaying) => set({ isPlaying }),
+  setCurrentSong: (currentSong) => set({ currentSong }),
+}));
+
